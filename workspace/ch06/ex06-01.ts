536fa574545437396896440034d0cd741f45378d
오서현 <bbibbi1118@naver.com>
2025-04-28 09:26:56 +0900

    🔥 파일 제거

--- workspace/ch06/ex06-01.ts
+++ workspace/ch06/ex06-01.ts
@@ -1,8 +1,8 @@
 // 개발 환경 확인
 
-(() => {
-  function hello(name: string): string {
-    return "Hello " + name;
+(()=>{
+  function hello(name: string): string{
+    return 'Hello ' + name;
   }
-  console.log(hello("TypeScript"));
+  console.log(hello('TypeScript'));
 })();
